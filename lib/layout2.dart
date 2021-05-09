7d712615b341ececcd019af42e5bed54c53d0319
Hiếu Phan <hida@Hieus-MacBook-Air.local>
2021-05-09 17:42:10 +0700

    make layout second

--- lib/layout2.dart
+++ lib/layout2.dart
@@ -5,17 +5,101 @@ class Layout2 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Layout 2"),
-      ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.pop(context);
-          },
-          child: Text('2: Go back!'),
+        appBar: AppBar(
+          title: Text("Layout 2"),
         ),
-      ),
-    );
+        body: Column(
+          children: <Widget>[
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Text(
+                    "color palette",
+                    style: TextStyle(
+                        fontFamily: 'Nimbus',
+                        fontSize: 40,
+                        color: Color.fromRGBO(112, 112, 112, 1),
+                        fontWeight: FontWeight.w300),
+                  ),
+                  Column(
+                    children: [
+                      Wrap(
+                        spacing: 20.0,
+                        runSpacing: 20.0,
+                        children: [
+                          Container(
+                            width: 73,
+                            height: 73,
+                            decoration: BoxDecoration(
+                                shape: BoxShape.circle,
+                                color: Color.fromRGBO(172, 169, 190, 1)),
+                          ),
+                          Container(
+                            width: 73,
+                            height: 73,
+                            decoration: BoxDecoration(
+                                shape: BoxShape.circle,
+                                color: Color.fromRGBO(225, 209, 222, 1)),
+                          ),
+                          Container(
+                            width: 73,
+                            height: 73,
+                            decoration: BoxDecoration(
+                                shape: BoxShape.circle,
+                                color: Color.fromRGBO(238, 227, 235, 1)),
+                          ),
+                          Container(
+                            width: 73,
+                            height: 73,
+                            decoration: BoxDecoration(
+                                shape: BoxShape.circle,
+                                color: Color.fromRGBO(246, 240, 244, 1)),
+                          ),
+                          Container(
+                            width: 73,
+                            height: 73,
+                            decoration: BoxDecoration(
+                                shape: BoxShape.circle,
+                                color: Color.fromRGBO(53, 64, 53, 1)),
+                          ),
+                          Container(
+                            width: 73,
+                            height: 73,
+                            decoration: BoxDecoration(
+                                shape: BoxShape.circle,
+                                color: Color.fromRGBO(60, 72, 58, 1)),
+                          ),
+                          Container(
+                            width: 73,
+                            height: 73,
+                            decoration: BoxDecoration(
+                                shape: BoxShape.circle,
+                                color: Color.fromRGBO(169, 170, 192, 1)),
+                          ),
+                          Container(
+                            width: 73,
+                            height: 73,
+                            decoration: BoxDecoration(
+                                shape: BoxShape.circle,
+                                color: Color.fromRGBO(186, 178, 199, 1)),
+                          ),
+                        ],
+                      )
+                    ],
+                  )
+                ],
+              ),
+            ),
+            Image.asset(
+              'assets/images/layout2.png',
+              height: 500,
+              width: double.infinity,
+              fit: BoxFit.cover,
+              alignment: Alignment.bottomLeft,
+            ),
+          ],
+        ));
   }
 }
